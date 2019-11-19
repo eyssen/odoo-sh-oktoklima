d0e81b4c7a8dd4a5c0c3c19e49aee3321ec43b89
Krisztian Eyssen <krisz@eyssen.hu>
2019-11-19 21:09:22 +0100

    octo price calculator

--- oktoklima/models/product.py
+++ oktoklima/models/product.py
@@ -3,6 +3,9 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
+import logging
+_logger = logging.getLogger(__name__)
+
 
 
 
@@ -13,7 +16,26 @@ class ProductTemplate(models.Model):
 
 
     family_id = fields.Many2one('product.family', u'Termékcsalád')
-
+    
+    
+    def _compute_okto_price(self):
+        
+        ProductTemplateS = self.env['product.template'].search([('seller_ids', '!=', False)])
+        for ProductTemplate in ProductTemplateS:
+            if ProductTemplate.seller_ids:
+                supplier_price = ProductTemplate.seller_ids[0].currency_id._convert(ProductTemplate.seller_ids[0].price, ProductTemplate.currency_id, self.env.user.company_id, fields.Date.today())
+                
+                if ProductTemplate.family_id:
+                    if ProductTemplate.family_id.discount > 0:
+                        standard_price = supplier_price * ProductTemplate.family_id.discount
+                    else:
+                        standard_price = supplier_price
+                else:
+                    standard_price = supplier_price
+                
+                ProductTemplate.standard_price = standard_price
+                ProductTemplate.list_price = standard_price * ProductTemplate.categ_id.list_price_margin
+            
 
 
 
@@ -31,3 +53,14 @@ class ProductFamily(models.Model):
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'A termékcsalád névnek egyedinek kell lennie!'),
     ]
+
+
+
+
+
+class ProductCategory(models.Model):
+    
+    _inherit = 'product.category'
+
+
+    list_price_margin = fields.Float(u'Eladási ár szorzó', default=0)
