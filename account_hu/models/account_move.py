5986426e9fe7b3a0e12684d7f4bf1d18d04ea004
Krisztian Eyssen <krisz@eyssen.hu>
2020-02-24 12:36:12 +0100

    fix

--- account_hu/models/account_move.py
+++ account_hu/models/account_move.py
@@ -140,6 +140,41 @@ class AccountMove(models.Model):
 
         return reverse_moves
 
+
+    @api.depends('line_ids.price_subtotal', 'line_ids.tax_base_amount', 'line_ids.tax_line_id', 'partner_id', 'currency_id')
+    def _compute_invoice_taxes_by_group(self):
+        ''' Helper to get the taxes grouped according their account.tax.group.
+        This method is only used when printing the invoice.
+        '''
+        for move in self:
+            lang_env = move.with_context(lang=move.partner_id.lang).env
+            tax_lines = move.line_ids.filtered(lambda line: line.tax_line_id)
+            res = {}
+            # There are as many tax line as there are repartition lines
+            done_taxes = set()
+            for line in tax_lines:
+                res.setdefault(line.tax_line_id.tax_group_id, {'base': 0.0, 'amount': 0.0})
+                res[line.tax_line_id.tax_group_id]['amount'] += line.price_subtotal
+                tax_key_add_base = tuple(move._get_tax_key_for_group_add_base(line))
+                if tax_key_add_base not in done_taxes:
+                    # The base should be added ONCE
+                    res[line.tax_line_id.tax_group_id]['base'] += line.tax_base_amount
+                    done_taxes.add(tax_key_add_base)
+                res[line.tax_line_id.tax_group_id]['percent'] = line.tax_line_id.amount
+                res[line.tax_line_id.tax_group_id]['description'] = line.tax_line_id.description
+            res = sorted(res.items(), key=lambda l: l[0].sequence)
+            move.amount_by_group = [(
+                group.name,
+                amounts['amount'],
+                amounts['base'],
+                formatLang(lang_env, amounts['amount'], currency_obj=move.currency_id),
+                formatLang(lang_env, amounts['base'], currency_obj=move.currency_id),
+                len(res),
+                group.id,
+                formatLang(lang_env, amounts['amount']+amounts['base'], currency_obj=move.currency_id),
+                amounts['description'],
+            ) for group, amounts in res]
+
     
     def _recalculate_kvtd_lines(self):
 
