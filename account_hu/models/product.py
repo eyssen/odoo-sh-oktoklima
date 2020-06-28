a45cc6432a6a7b9fa0cb420bfa59317bb72909a9
Krisztian Eyssen <krisz@eyssen.hu>
2020-06-28 22:00:49 +0200

    account_hu

--- account_hu/models/product.py
+++ account_hu/models/product.py
@@ -9,16 +9,6 @@ _logger = logging.getLogger(__name__)
 
 
 
-KVTDTYPE = [
-    ('Akt.', 'Akkumulátor környezetvédelmi termékdíja'),
-    ('Cskt.', 'Csomagolási kv. termékdíja'),
-    ('Kkt.', 'Egyéb kőolajtermékek kv. termékdíja'),
-    ('Gkt.', 'Gumiabroncs kv. termékdíja'),
-    ('Hkkt.', 'Hűtőközeg kv. termékdíja'),
-    ('Pkt.', 'Reklámhordozó papírok kv. termékdíja'),
-    ('Ekt.', 'Elektromos és elektronikai berendezések kv. termékdíja')
-]
-
 class product_template(models.Model):
     
     _inherit = "product.template"
@@ -26,7 +16,6 @@ class product_template(models.Model):
     list_price_gross = fields.Float(u'Bruttó eladási ár', compute='_compute_gross_price', readonly=True)
     vtsz_id = fields.Many2one('vtsz', string="VTSZ")
     szj_id = fields.Many2one('szj', string="SZJ")
-    kvtd_ids = fields.One2many('product.kvtd', 'product_tmpl_id', u'KVTD')
 
 
     @api.depends('list_price', 'taxes_id')
@@ -60,41 +49,3 @@ class szj(models.Model):
     name = fields.Char('SZJ', required=True)
     description = fields.Char('Megnevezés')
     product_template_ids = fields.One2many('product.template', 'szj_id', string="Termékek")
-
-
-
-
-
-class kvtd(models.Model):
-    
-    _name = "kvtd"
-    
-    
-    name = fields.Char(u'Megnevezés', required=True)
-    code = fields.Char(u'KT/CSK kód', required=True)
-    type = fields.Selection(KVTDTYPE, u'Termékdíj típus', required=True)
-    vtsz_id = fields.Many2one('vtsz', string="VTSZ")
-    rate = fields.Float(u'Díjtétel (Ft/kg)', required=True)
-
-    
-    _sql_constraints = [
-        ('name_uniq', 'unique(name)', 'A megnevezésnek egyedinek kell lennie!'),
-    ]
-
-
-
-
-
-class product_kvtd(models.Model):
-
-    _name = "product.kvtd"
-    
-    
-    product_tmpl_id = fields.Many2one('product.template', u'Termék', required=True)
-    kvtd_id = fields.Many2one('kvtd', u'Termékdíj fajtája', required=True)
-    weight = fields.Float(u'Mennyiség (kg)', required=True)
-
-    
-    _sql_constraints = [
-        ('product_kvtd_uniq', 'unique(product_tmpl_id, kvtd_id)', 'Egy termékdíj fajta csak egyszer szerepelhet egy terméknél!'),
-    ]
