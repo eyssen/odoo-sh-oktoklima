f414dc5d3a59da5644121a3cb77d1930c21564e3
Krisztian Eyssen <krisz@eyssen.hu>
2020-07-05 17:26:32 +0200

    sale_advance_plus

---
+++ sale_advance_plus/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': u'Sale Advence Plus',
+    'description': u'Sale Advence Plus',
+    'category': 'Accounting',
+    'version': '1.0',
+    'author': 'eYssen IT Services',
+    'website': 'https://eyssen.hu/',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
